464a4fa303a0a2bc25721d251527b8227702754e
Cleverson Biora <cleverson.biora@gmail.com>
2019-04-26 00:01:38 -0300

    Refit

--- GitHub.Domain/Models/Sample.cs
+++ GitHub.Domain/Models/Sample.cs
@@ -25,9 +25,6 @@ namespace GitHub.Domain.Models
         [JsonProperty("avatar_url")]
         public string AvatarUrl { get; set; }
 
-        [JsonProperty("gravatar_id")]
-        public string GravatarId { get; set; }
-
         [JsonProperty("url")]
         public string Url { get; set; }
 
@@ -40,32 +37,8 @@ namespace GitHub.Domain.Models
         [JsonProperty("following_url")]
         public string FollowingUrl { get; set; }
 
-        [JsonProperty("gists_url")]
-        public string GistsUrl { get; set; }
-
-        [JsonProperty("starred_url")]
-        public string StarredUrl { get; set; }
-
-        [JsonProperty("subscriptions_url")]
-        public string SubscriptionsUrl { get; set; }
-
-        [JsonProperty("organizations_url")]
-        public string OrganizationsUrl { get; set; }
-
         [JsonProperty("repos_url")]
         public string ReposUrl { get; set; }
-
-        [JsonProperty("events_url")]
-        public string EventsUrl { get; set; }
-
-        [JsonProperty("received_events_url")]
-        public string ReceivedEventsUrl { get; set; }
-
-        [JsonProperty("type")]
-        public string Type { get; set; }
-
-        [JsonProperty("site_admin")]
-        public bool SiteAdmin { get; set; }
     }
 
     public class Repository
@@ -101,29 +74,8 @@ namespace GitHub.Domain.Models
         [JsonProperty("url")]
         public string Url { get; set; }
 
-        [JsonProperty("forks_url")]
-        public string ForksUrl { get; set; }
-
-        [JsonProperty("keys_url")]
-        public string KeysUrl { get; set; }
-
         [JsonProperty("collaborators_url")]
         public string CollaboratorsUrl { get; set; }
-
-        [JsonProperty("teams_url")]
-        public string TeamsUrl { get; set; }
-
-        [JsonProperty("hooks_url")]
-        public string HooksUrl { get; set; }
-
-        [JsonProperty("issue_events_url")]
-        public string IssueEventsUrl { get; set; }
-
-        [JsonProperty("events_url")]
-        public string EventsUrl { get; set; }
-
-        [JsonProperty("assignees_url")]
-        public string AssigneesUrl { get; set; }
 
         [JsonProperty("branches_url")]
         public string BranchesUrl { get; set; }
@@ -131,82 +83,10 @@ namespace GitHub.Domain.Models
         [JsonProperty("tags_url")]
         public string TagsUrl { get; set; }
 
-        [JsonProperty("blobs_url")]
-        public string BlobsUrl { get; set; }
-
-        [JsonProperty("git_tags_url")]
-        public string GitTagsUrl { get; set; }
-
-        [JsonProperty("git_refs_url")]
-        public string GitRefsUrl { get; set; }
-
-        [JsonProperty("trees_url")]
-        public string TreesUrl { get; set; }
-
-        [JsonProperty("statuses_url")]
-        public string StatusesUrl { get; set; }
-
         [JsonProperty("languages_url")]
         public string LanguagesUrl { get; set; }
 
-        [JsonProperty("stargazers_url")]
-        public string StargazersUrl { get; set; }
-
-        [JsonProperty("contributors_url")]
-        public string ContributorsUrl { get; set; }
-
-        [JsonProperty("subscribers_url")]
-        public string SubscribersUrl { get; set; }
-
-        [JsonProperty("subscription_url")]
-        public string SubscriptionUrl { get; set; }
-
-        [JsonProperty("commits_url")]
-        public string CommitsUrl { get; set; }
-
-        [JsonProperty("git_commits_url")]
-        public string GitCommitsUrl { get; set; }
-
-        [JsonProperty("comments_url")]
-        public string CommentsUrl { get; set; }
-
-        [JsonProperty("issue_comment_url")]
-        public string IssueCommentUrl { get; set; }
-
-        [JsonProperty("contents_url")]
-        public string ContentsUrl { get; set; }
-
-        [JsonProperty("compare_url")]
-        public string CompareUrl { get; set; }
-
-        [JsonProperty("merges_url")]
-        public string MergesUrl { get; set; }
-
-        [JsonProperty("archive_url")]
-        public string ArchiveUrl { get; set; }
-
         [JsonProperty("downloads_url")]
         public string DownloadsUrl { get; set; }
-
-        [JsonProperty("issues_url")]
-        public string IssuesUrl { get; set; }
-
-        [JsonProperty("pulls_url")]
-        public string PullsUrl { get; set; }
-
-        [JsonProperty("milestones_url")]
-        public string MilestonesUrl { get; set; }
-
-        [JsonProperty("notifications_url")]
-        public string NotificationsUrl { get; set; }
-
-        [JsonProperty("labels_url")]
-        public string LabelsUrl { get; set; }
-
-        [JsonProperty("releases_url")]
-        public string ReleasesUrl { get; set; }
-
-        [JsonProperty("deployments_url")]
-        public string DeploymentsUrl { get; set; }
     }
 }
